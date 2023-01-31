38900f8794621eaf9cfd79aa32661e5017e1f063
trainerdev <fatihcane0696@outlook.com>
2023-01-31 13:29:25 +0300

    create landing page with splash screen editing

--- app/src/main/java/com/fcadev/movieapp/SplashScreen.kt
+++ app/src/main/java/com/fcadev/movieapp/SplashScreen.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val landingImg : ImageView = findViewById(R.id.landingPageImg)
+        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        landingImg.startAnimation(sideAnimation)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
